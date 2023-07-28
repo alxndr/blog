25ca5df1b5e5316d5e22ce234f467a1bd31d87b7
Alexander <alxndr+github@gmail.com>
2023-07-28 11:31:01 -0700

    ArrowJS working in browser

---
+++ _drafts/sourdough-pizza-dough-ratio-calculator.md
@@ -0,0 +1,15 @@
+---
+title: sourdough pizza dough ratio calculator
+---
+
+I've been tending to some sourdough starter ever since the Great Pandemic of 2020. I'll often make pizza dough with the "leftover" starter.
+
+Below I've created an interactive tool to let you enter amounts of starter, water, and flour (and optionally yeast and salt too), and then receive a precise-as-you-like set of numbers describing the ratios you'd entered.
+
+(Trial and error has landed me at 1 part starter, 4 parts room-temp water, 5 parts high-protein flour, 0.1 part salt. I built this tool in order to help me fine-tune that ratio, and determine ratios for using yeast.)
+
+<noscript>The interactivity below requires client-side JavaScript. Not common for me, I know, but that's all I've got for readable content in this blog post!</noscript>
+
+{% include dough-ratio-calculator.js %}
+
+<div id="dough-ratio-calculator"></div>
---
+++ _includes/dough-ratio-calculator.js
@@ -0,0 +1,36 @@
+<script type="module">
+import { reactive, html } from 'https://esm.sh/@arrow-js/core';
+
+function int(n) {
+  return n.toPrecision(2);
+}
+function pct(n) {
+  return (n*100).toPrecision(3)+'%';
+}
+
+const root$ = document.getElementById('dough-ratio-calculator');
+const d = reactive({ S: 32.5, W: 156.5, F: 192.5, Y: 0.5, T: 3.5, });
+html`
+  <ul>
+    <li><input value="${() => d.S}" @input="${e => { d.S = e.target.value }}" /> starter
+    <li><input value="${() => d.W}" @input="${e => { d.W = e.target.value }}" /> water
+    <li><input value="${() => d.F}" @input="${e => { d.F = e.target.value }}" /> flour
+    <li><input value="${() => d.Y}" @input="${e => { d.Y = e.target.value }}" /> yeast
+    <li><input value="${() => d.T}" @input="${e => { d.T = e.target.value }}" /> salt
+  </ul>
+  <table><caption>Bakers Percentages</caption>
+    <tr><td>starter:</td><td>${() => pct(d.S/d.F)}</td></tr>
+    <tr>  <td>water:</td><td>${() => pct(d.W/d.F)}</td></tr>
+    <tr>  <td>flour:</td><td>100% always</td>          </tr>
+    <tr>  <td>yeast:</td><td>${() => pct(d.Y/d.F)}</td></tr>
+    <tr>   <td>salt:</td><td>${() => pct(d.T/d.F)}</td></tr>
+  </table>
+  <p>ratios by starter: <abbr title="starter">1</abbr>
+    : <abbr title="water">${() => int(d.W/d.S)}</abbr>
+    : <abbr title="flour">${() => int(d.F/d.S)}</abbr>
+    : <abbr title="yeast">${() => pct(d.Y/d.S)}</abbr>
+    : <abbr title="salt" >${() => pct(d.T/d.S)}</abbr>
+  </p>
+`(root$);
+
+</script>
